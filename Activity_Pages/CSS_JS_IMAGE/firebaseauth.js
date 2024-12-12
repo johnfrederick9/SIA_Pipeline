d73651ab79c27619ac0030e88133f2a762b7cccb
johnfrederick9 <gelayjohnfrederick0@gmail.com>
2024-12-13 07:51:30 +0800

    Creating firebaseauth.js

--- Activity_Pages/CSS_JS_IMAGE/firebaseauth.js
+++ Activity_Pages/CSS_JS_IMAGE/firebaseauth.js
@@ -1,8 +1,13 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Import Firebase modules
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
