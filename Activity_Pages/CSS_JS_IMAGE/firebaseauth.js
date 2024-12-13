a7d1376ac04c06191cf1b46fa96f8295abc87b25
johnfrederick9 <gelayjohnfrederick0@gmail.com>
2024-12-13 09:36:30 +0800

    Creating otp.html

--- Activity_Pages/CSS_JS_IMAGE/firebaseauth.js
+++ Activity_Pages/CSS_JS_IMAGE/firebaseauth.js
@@ -92,7 +92,7 @@ async function sendOtpEmail(recipientName, recipientEmail, otp) {
   };
 
   try {
-    const response = await emailjs.send("SERVICE_EMAILJS", "TEMPLATE_EMAILJS", templateParams);
+    const response = await emailjs.send("service_evq566f", "template_10o7zwy", templateParams);
     console.log(`OTP email sent successfully to: ${recipientEmail}`);
     showMessage("OTP sent to your email. Please verify.", "signInMessage");
     return true; // Indicate success
